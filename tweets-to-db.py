e9fbfc45cb20b529cf275687d654c0b030410483
rjp <rjpartington@gmail.com>
2014-05-30 09:34:59 +0200

    Rely on SQLite to handle the timezone parsing

--- tweets-to-db.py
+++ tweets-to-db.py
@@ -5,6 +5,10 @@ import sys
 import os
 import sys
 import codecs
+from datetime import datetime
+import pytz
+
+utc = pytz.utc
 
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
@@ -15,12 +19,10 @@ conn = sqlite3.connect(dbfile)
 c = conn.cursor()
 
 with open(tweets, encoding="utf-8") as file:
-    print(file)
     for line in file:
-        print(line.split(' ', 5))
-        (id, date, time, timezone, user, rest) = line.split(' ', 5)
-        print("%s,%s %s %s,[%s]" % (id, date, time, timezone, rest))
+        (id, datetimetz, user, rest) = line.split(' ', 3)
+        print("%s,%s,[%s]" % (id, datetimetz, rest))
         tweet_text = rest.rstrip('\r\n')
-        c.execute('insert or ignore into tweets values (?,?,?)', [id, date+' '+time+' '+timezone, tweet_text])
+        c.execute('insert or ignore into tweets values (?,?,?)', [id, datetimetz, tweet_text])
     conn.commit()
 conn.close()
