f071ec2d998d27f199c5d780005ce34984b8297a
rjp <rjpartington@gmail.com>
2014-05-30 09:34:06 +0200

    Allow a single additional file containing distances

--- update-distances.py
+++ update-distances.py
@@ -9,7 +9,9 @@ import codecs
 
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
+extras = None
 dbfile = sys.argv[1]
+if len(sys.argv) > 2: extras = sys.argv[2]
 
 conn = sqlite3.connect(dbfile)
 c = conn.cursor()
@@ -29,4 +31,11 @@ for tweet_data in c.fetchall():
         distance = overrides.get(tweet_id, float(m.group(1)))
         c.execute("insert or ignore into distances (tweet_id, distance, date) values (?,?,?)", [tweet_id, distance, tweet_date])
 
+if extras is not None:
+    with open(extras, encoding="utf-8") as file:
+        for line in file:
+            (tweet_id, tweet_date, distance) = line.split(' ', 2)
+            print("%s,%s,[%s]" % (id, tweet_date, distance))
+            distance = distance.rstrip('\r\n')
+            c.execute("insert or ignore into distances (tweet_id, distance, date) values (?,?,?)", [tweet_id, distance, tweet_date])
 conn.commit()
