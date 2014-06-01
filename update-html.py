2ae68ead66d28cdb32fcae0933d9f7e2bba6f303
rjp <rjpartington@gmail.com>
2014-06-02 00:44:49 +0200

    Much HTML, very wow, such ...
    Update bootstrap.html to have more data
    Add `span_replacer` to DOM-template spans from a dict
    Have an epiphany that this is how all NIH template systems begin
    Have a little cry

--- update-html.py
+++ update-html.py
@@ -14,6 +14,9 @@ conn = sqlite3.connect(dbfile)
 c = conn.cursor()
 c.execute("select datetime(date), distance, tweet_id from distances order by date desc")
 distances = c.fetchall()
+
+c.execute("select count(1), min(distance), max(distance) from distances")
+(count_dist, min_dist, max_dist) = c.fetchone()
 conn.close()
 
 twitbase = "https://twitter.com/RTWbike/status/%s"
@@ -28,8 +31,18 @@ def clone_row_with_dict(bsrow):
         del(i['id'])
     return q, a
 
+def span_replacer(template, dict):
+    for i,j in dict.items():
+#        x = template.find("span", id="repl.%s" % (i))
+        for x in template.find_all("span", id="repl.%s" % (i)):
+            x.string = template.new_string(str(j))
+
+with open("progressbar.html") as file:
+    progress_template = file.read()
+
 with open(input_html) as file:
     soup = BeautifulSoup(file.read())
+    span_replacer(soup, {"count": count_dist, "min": min_dist, "max": max_dist})
     target = soup.find("tbody", id="distances")
     row = target.find("tr", id="raw-data").extract()
     for d in distances:
@@ -40,7 +53,11 @@ with open(input_html) as file:
         twit_url = twitbase % (d[2])
         twit_a = soup.new_tag("a", href=twit_url)
         twit_a.string = str(d[2])
+        progwidth = 100*d[1]/max_dist
         a['row.tweet'].append(twit_a)
+        progress = BeautifulSoup(progress_template % (d[1], progwidth, progwidth))
+        a["row.progress"].append(progress)
         target.append(q)
     with open(output_html, "w", encoding="utf-8") as output:
         print(soup, file=output)
+
