8f0b20e667fc248aa538ed0fdf53c78f0724ef3b
rjp <rjpartington@gmail.com>
2014-07-31 23:57:03 +0200

    Minor tweaks to the DOM-replacement HTML code
    - Fetch more things from the db for templatising
    - Wrap all replacers in `try`/`except` because it's easier
    TODO: extract this logic into some kind of library

--- update-html.py
+++ update-html.py
@@ -15,8 +15,21 @@ c = conn.cursor()
 c.execute("select datetime(date), distance, tweet_id from distances order by date desc")
 distances = c.fetchall()
 
-c.execute("select count(1), min(distance), max(distance) from distances")
-(count_dist, min_dist, max_dist) = c.fetchone()
+c.execute("select count(1), min(distance), max(distance), sum(distance) from distances")
+(count_dist, min_dist, max_dist, total_dist) = c.fetchone()
+
+nice_total_dist = "%d" % total_dist
+
+c.execute("select set_date, eddington from eddington order by set_date desc limit 1")
+(e_date, eddington) = c.fetchone()
+
+next_counts = []
+for next_e in range(eddington + 1, eddington + 6):
+    c.execute("select count(1), 42 from distances where distance >= ?", [next_e])
+    (howmanydone, junk) = c.fetchone()
+    howmany = next_e - howmanydone
+    next_counts.append([next_e, howmany])
+
 conn.close()
 
 twitbase = "https://twitter.com/RTWbike/status/%s"
@@ -34,30 +47,50 @@ def clone_row_with_dict(bsrow):
 def span_replacer(template, dict):
     for i,j in dict.items():
 #        x = template.find("span", id="repl.%s" % (i))
-        for x in template.find_all("span", id="repl.%s" % (i)):
-            x.string = template.new_string(str(j))
+        try:
+            for x in template.find_all("span", id="repl.%s" % (i)):
+                x.string = template.new_string(str(j))
+        except:
+            pass
+
+next_eddington = next_counts[0][0]
+howmany = next_counts[0][1]
 
 with open("progressbar.html") as file:
     progress_template = file.read()
 
 with open(input_html) as file:
     soup = BeautifulSoup(file.read())
-    span_replacer(soup, {"count": count_dist, "min": min_dist, "max": max_dist})
+    span_replacer(soup, {"count": count_dist, "min": min_dist, "max": max_dist, "total": nice_total_dist})
+    span_replacer(soup, {"eddington": eddington, "when": e_date, "next": next_eddington, "howmany": howmany})
+    target = soup.find("tbody", id="eddingtons")
+    try:
+        row = target.find("tr", id="eddings").extract()
+        for e in next_counts:
+            q, a = clone_row_with_dict(row)
+            a['row.next_ed'].string = "%d" % (e[0])
+            a['row.howmany'].string = "%d" % (e[1])
+            target.append(q)
+    except:
+        pass
     target = soup.find("tbody", id="distances")
-    row = target.find("tr", id="raw-data").extract()
-    for d in distances:
-        q, a = clone_row_with_dict(row)
-        tds = q("td")
-        a['row.utc'].string = str(d[0])
-        a['row.distance'].string = "%.1f" % (d[1])
-        twit_url = twitbase % (d[2])
-        twit_a = soup.new_tag("a", href=twit_url)
-        twit_a.string = str(d[2])
-        progwidth = 100*d[1]/max_dist
-        a['row.tweet'].append(twit_a)
-        progress = BeautifulSoup(progress_template % (d[1], progwidth, progwidth))
-        a["row.progress"].append(progress)
-        target.append(q)
+    try:
+        row = target.find("tr", id="raw-data").extract()
+        for d in distances:
+            q, a = clone_row_with_dict(row)
+            tds = q("td")
+            a['row.utc'].string = str(d[0])
+            a['row.distance'].string = "%.1f" % (d[1])
+            twit_url = twitbase % (d[2])
+            twit_a = soup.new_tag("a", href=twit_url)
+            twit_a.string = str(d[2])
+            progwidth = 100*d[1]/max_dist
+            a['row.tweet'].append(twit_a)
+            progress = BeautifulSoup(progress_template % (d[1], progwidth, progwidth))
+            a["row.progress"].append(progress)
+            target.append(q)
+    except:
+        pass
     with open(output_html, "w", encoding="utf-8") as output:
         print(soup, file=output)
 
